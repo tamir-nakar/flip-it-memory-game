b0351d604bbdb70770042c8784a0ee81dd42d726
tamirn <nakar.tamir@gmail.com>
2024-04-20 19:16:28 +0300

    style(logs):

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 
 const redis = Redis.fromEnv();
 app.use(cors());
-
+//
 // app.use(cors({
 //   origin: "https://flip-it-memory-game.vercel.app",
 //   methods: ["GET", "POST"],
